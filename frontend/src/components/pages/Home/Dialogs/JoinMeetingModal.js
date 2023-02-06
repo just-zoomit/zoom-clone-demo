16dec391357e16b88aa3e985e16dddfe5cfac35f
Donte Small <donte.small@zoom.us>
2023-02-05 21:51:17 -0500

    Fixed Join Button

--- frontend/src/components/pages/Home/Dialogs/JoinMeetingModal.js
+++ frontend/src/components/pages/Home/Dialogs/JoinMeetingModal.js
@@ -28,7 +28,6 @@ export const JoinMeetingModal = ({ setShowModal }) => {
       if (id.current.value === "" || password.current.value === "") {
         alert("Please enter a valid meeting ID and password");
       } else {
-        console.log("id", id.current.value);
         setShowModal(false);
         navigate(`/msdk/?mn=${id.current.value}&pw=${password.current.value}`);
       }
@@ -41,15 +40,13 @@ export const JoinMeetingModal = ({ setShowModal }) => {
   return ReactDom.createPortal(
     <div className="container" ref={modalRef} onClick={closeModal}>
       <div className="modal">
-       
         <button onClick={() => setShowModal(false)}>X</button>
 
         <div>
-        <h2>Join Meeting</h2>
+          <h2>Join Meeting</h2>
 
           <form onSubmit={handleSubmit}>
             <div>
-            
               <input
                 type="text"
                 id="topic"
@@ -57,11 +54,9 @@ export const JoinMeetingModal = ({ setShowModal }) => {
                 ref={id}
                 required={true}
               />
-              
             </div>
             &nbsp; &nbsp;
             <div>
-            
               <input
                 type="text"
                 id="topic"
@@ -69,18 +64,20 @@ export const JoinMeetingModal = ({ setShowModal }) => {
                 ref={password}
                 required={true}
               />
-            
             </div>
             <hr class="solid"></hr>
-
-            <div style={{ position: "relative", marginTop:"auto", display: "flex" , justifyContent: "space-between"}} >
-              
-              <button type="submit" style={{ background:"#316efd" }}>
-              {role === 1 ? "Join Meeting" : "Start Meeting"}
-            </button>
-
-          </div>
-            
+            <div
+              style={{
+                position: "relative",
+                bottom: "0",
+                right: "0",
+                width: "300px",
+              }}
+            >
+              <button type="submit" style={{ background: "#316efd" }}>
+                {role === 1 ? "Join Meeting" : "Start Meeting"}
+              </button>
+            </div>
           </form>
         </div>
       </div>
